9335b80e237a4fbaab9ca20de353523846129350
Nolan Taft <killer123yo@gmail.com>
2024-08-06 22:06:48 -0400

    Delete testcases directory

--- testcases/README.md
+++
@@ -1,2 +0,0 @@
-# Important
-To run these test cases, you will have to move them into the source code directory, as they depend on the modules I've created. They will not function standalone.
--- testcases/partition_resize_test.py
+++
@@ -1,8 +0,0 @@
-from PartitionManager import PartitionManager
-from BytesConverter import BytesManager
-
-bytemanager = BytesManager()
-
-partManager = PartitionManager()
-
-partManager.modifyPartition("testUser", bytemanager.ByteDetection("1gb"), "SIZETO")
